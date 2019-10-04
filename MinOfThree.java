6a43634cb8694e29eed63ba04a656da473a42c5a
Garegin H <HovhannG@wlhs.wlwv.k12.or.us>
2019-10-04 08:58:57 -0700

    Third commit

---
+++ MinOfThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MinOfThree{
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please input three numbers");
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+    int num3 = scan.nextInt();
+    if (num1<=num2 && num1<=num3)
+      System.out.println("Minimum is "+num1);
+    else if(num2<=num1 && num2<=num3)
+      System.out.println("Minimum is "+num2);
+    else if(num3<=num1 && num3<=num2)
+      System.out.println("Minimum is "+num3);
+  }
+}
